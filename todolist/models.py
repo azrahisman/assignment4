6753c1fc47ffc058f2fec656b33e3648ff2373e9
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-10-05 19:02:23 +0700

    re-attempt

--- todolist/models.py
+++ todolist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
     date = models.DateField()
     title = models.CharField(max_length=50)
     description = models.TextField()
