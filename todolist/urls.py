65d7543d62bf43e4d8f5d4147a361454dcd5c1c5
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-10-05 19:31:03 +0700

    re-attempt

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from todolist.views import show_todolist
-from todolist.views import register
-from todolist.views import login_user
-from todolist.views import logout_user  # Customize with the name of the function created
 
 app_name = 'todolist'
 
 urlpatterns = [
     path('', show_todolist, name='show_todolist'),
-    path('register/', register, name='register'),
 ]
