ccb59998530dcd1bd144121e9de4c0d11c84e2e0
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-10-05 19:29:17 +0700

    re-attempt

--- todolist/urls.py
+++ todolist/urls.py
@@ -9,6 +9,4 @@ app_name = 'todolist'
 urlpatterns = [
     path('', show_todolist, name='show_todolist'),
     path('register/', register, name='register'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),  # Customize with the name of the function created
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -17,40 +17,5 @@ def show_todolist(request):
     context = {
         'list_item': data_todolist_item,
         'name': 'Azra',
-        'last_login': request.COOKIES['last_login']
     }
     return render(request, "todolist.html", context)
-
-def register(request):
-    form = UserCreationForm()
-
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Account successfully created!')
-            return redirect('todolist:login')
-
-    context = {'form':form}
-    return render(request, 'register.html', context)
-
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user) # login first
-            response = HttpResponseRedirect(reverse("todolist:show_todolist")) # create response
-            response.set_cookie('last_login', str(datetime.datetime.now())) # create last_login cookie and add it to response
-            return response
-        else:
-            messages.info(request, 'Wrong Username or Password!')
-    context = {}
-    return render(request, 'login.html', context)
-
-def logout_user(request):
-    logout(request)
-    response = HttpResponseRedirect(reverse('todolist:login'))
-    response.delete_cookie('last_login')
-    return response
